0d515cae44adb4afeb669fc63a1f6165017ca249
Mungmiiing <watcharee.issarasaena@gmail.com>
2024-11-10 15:19:09 +0700

    Update app_predict_penguin_ID65130700333.py

--- app_predict_penguin_ID65130700333.py
+++ app_predict_penguin_ID65130700333.py
@@ -10,15 +10,15 @@ with open('model_penguin_65130700333.pkl', 'rb') as file:
 st.title("Penguin Species Prediction")
 
 # Get user input
-island = st.selectbox("Island", ["Torgersen", "Biscoe", "Dream"])
-culmen_length = st.number_input("Culmen Length (mm)", min_value=0.0, max_value=100.0, value=37.0)
-culmen_depth = st.number_input("Culmen Depth (mm)", min_value=0.0, max_value=100.0, value=19.3)
-flipper_length = st.number_input("Flipper Length (mm)", min_value=0.0, max_value=300.0, value=192.3)
-body_mass = st.number_input("Body Mass (g)", min_value=0.0, max_value=6000.0, value=3750)
-sex = st.selectbox("Sex", ["MALE", "FEMALE"])
+#island = st.selectbox("Island", ["Torgersen", "Biscoe", "Dream"])
+#culmen_length = st.number_input("Culmen Length (mm)", min_value=0.0, max_value=100.0, value=37.0)
+#culmen_depth = st.number_input("Culmen Depth (mm)", min_value=0.0, max_value=100.0, value=19.3)
+#flipper_length = st.number_input("Flipper Length (mm)", min_value=0.0, max_value=300.0, value=192.3)
+#body_mass = st.number_input("Body Mass (g)", min_value=0.0, max_value=6000.0, value=3750)
+#sex = st.selectbox("Sex", ["MALE", "FEMALE"])
 
 # Prepare input data
-x_new = pd.DataFrame({'island': [island],
+#x_new = pd.DataFrame({'island': [island],
                        'culmen_length_mm': [culmen_length],
                        'culmen_depth_mm': [culmen_depth],
                        'flipper_length_mm': [flipper_length],
@@ -26,7 +26,7 @@ x_new = pd.DataFrame({'island': [island],
                        'sex': [sex]})
 
 # Predict the species
-if st.button("Predict"):
-    y_pred_new = model.predict(x_new)
-    predicted_species = species_encoder.inverse_transform(y_pred_new)[0]
-    st.write("Predicted Species:", predicted_species)
+#if st.button("Predict"):
+    #y_pred_new = model.predict(x_new)
+    #predicted_species = species_encoder.inverse_transform(y_pred_new)[0]
+    #st.write("Predicted Species:", predicted_species)
